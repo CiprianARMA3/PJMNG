0116675d83621919fda27ae301ebfc5e7d898bcd
cipriankali <cipriankali@ciprian-kali>
2025-11-13 10:06:52 +0100

    config

--- frontend/utils/supabase/admin.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// NEVER use this on the client in production!
-export const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-)
--- frontend/utils/supabase/server.ts
+++ frontend/utils/supabase/server.ts
@@ -1,27 +1,29 @@
-import { createServerClient } from "@supabase/ssr";
+
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export function createClient() {
-  const cookieStore = cookies();
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    supabaseUrl!,
+    supabaseKey!,
     {
       cookies: {
         async getAll() {
-          return (await cookieStore).getAll();
+          return (await cookieStore).getAll()
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(async ({ name, value, options }) =>
-              (await cookieStore).set(name, value, options)
-            );
+            cookiesToSet.forEach(async ({ name, value, options }) => (await cookieStore).set(name, value, options))
           } catch {
-            // ignore if headers are already sent
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
           }
         },
       },
-    }
+    },
   );
-}
+};
